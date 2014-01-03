5853de41870f205d6d0bc83c0fcd0b937c71a6aa
Tom Bell <tomb@tomb.io>
2014-01-03 01:35:36 +0000

    Add implementation and tests

--- dye.go
+++ dye.go
@@ -1,1 +1,39 @@
 package dye
+
+import (
+	"bytes"
+	"os/exec"
+	"strings"
+)
+
+var bin = "pygmentize"
+
+func Binary(path string) error {
+	if _, err := exec.LookPath(path); err != nil {
+		return err
+	}
+
+	bin = path
+	return nil
+}
+
+func Which() string {
+	return bin
+}
+
+func Highlight(code string, lexer string, format string, enc string) (string, error) {
+	cmd := exec.Command(bin, "-l"+lexer, "-f"+format, "-O encoding="+enc)
+	cmd.Stdin = strings.NewReader(code)
+
+	var stdout bytes.Buffer
+	cmd.Stdout = &stdout
+
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+
+	if err := cmd.Run(); err != nil {
+		return "", err
+	}
+
+	return stdout.String(), nil
+}
--- dye_test.go
+++ dye_test.go
@@ -1,1 +1,31 @@
 package dye
+
+import (
+	"testing"
+)
+
+func expect(t *testing.T, a interface{}, b interface{}) {
+	if a != b {
+		t.Errorf("Expected %v, got %v", b, a)
+	}
+}
+
+func refute(t *testing.T, a interface{}, b interface{}) {
+	if a == b {
+		t.Errorf("Did not expect %v, got %v", b, a)
+	}
+}
+
+func Test_Dye_Binary(t *testing.T) {
+	Binary("/usr/local/bin/pygmentize")
+}
+
+func Test_Dye_Which(t *testing.T) {
+	which := Which()
+	expect(t, which, "/usr/local/bin/pygmentize")
+}
+
+func Test_Dye_Highlight(t *testing.T) {
+	output, _ := Highlight("func main() {}", "go", "html", "utf-8")
+	expect(t, output, "<div class=\"highlight\"><pre><span class=\"kd\">func</span> <span class=\"nx\">main</span><span class=\"p\">()</span> <span class=\"p\">{}</span>\n</pre></div>\n")
+}
